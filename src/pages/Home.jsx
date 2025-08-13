ec74d1bf40a5b5122f05aca1f19d56bf18ba9bdd
melodykellynwaogu <melodykellynwaogu224@gmail.com>
2025-08-13 16:50:27 +0200

    Final before deploy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import SearchBar from '../components/SearchBar';
+import SearchBar from '../components/SearchBar.jsx';
 import RecipeList from '../components/RecipeList';
 import CartChat from '../components/CartChat';
 
